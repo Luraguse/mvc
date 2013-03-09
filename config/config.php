597030e8ee59847b47b6dc6f5ccc957826e8ef46
Luis Gutierrez <cinema.nightmare@gmail.com>
2013-03-08 17:26:07 -0700

    Resetear config

---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+// Definicion de constantes
+
+define ('DB_NAME', '');
+define ('DB_USER', '');
+define ('DB_PASSWORD', '');
+define ('DB_HOST', '');
+
+define ('BASE_PATH','');
+define ('JS_FULL_PATH',    '');
+define ('CSS_FULL_PATH',   '');
+define ('JS_PUBLIC_PATH',  '');
+define ('CSS_PUBLIC_PATH', '');
+
+define ('PAGINATE_LIMIT', '5');
+
+// Defaults
+define('DEFAULT_CONTROLLER','');
+define('DEFAULT_ACTION','');
