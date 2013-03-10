9df6f3140c8d4d228a32708098410eb552bb20f4
Luis Gutierrez <cinema.nightmare@gmail.com>
2013-03-10 12:11:11 -0600

    Remover modelos controladores y vistas de proyecto pasado

--- application/views/footer.php
+++
@@ -1,8 +0,0 @@
-        </div>
-        <footer id="main_footer" class="dark_grey">
-            <h1>
-                Powered by <a href="http://hitosoft.com"><img alt="Hitosoft" title="Hitosoft" src="<?php echo BASE_PATH."/public/" ?>images/logo_hitosoft.png"></a>
-            </h1>
-        </footer>
-    </body>
-</html>
